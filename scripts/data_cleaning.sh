92eebe9a0c7e53666d4ed8db636c8fa55e1c1be5
Devesh Singh <100996117+deveshs18@users.noreply.github.com>
2025-02-26 23:12:06 -0800

    data_cleaning.sh

--- scripts/data_cleaning.sh
+++ scripts/data_cleaning.sh
@@ -1,1 +1,7 @@
 
+emove headers and fix log formatting
+awk -F, 'NR==1 {print "IP,Time,URL,Status"; next} 
+{gsub(/\[|\]/,"",$2); gsub(/ HTTP.*/,"",$3); print $1","$2","$3","$4}' weblog.csv > cleaned_weblog.csv
+
+echo "Log file cleaned successfully. Output: cleaned_weblog.csv"
+
